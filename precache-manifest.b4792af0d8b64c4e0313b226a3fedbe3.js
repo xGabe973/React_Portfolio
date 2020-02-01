3818ced0fedcf59573ece00c68cd07edb5ece407
xGabe973 <x973.alvarado@gmail.com>
2020-02-01 17:16:18 -0500

    Updates

---
+++ precache-manifest.b4792af0d8b64c4e0313b226a3fedbe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec69e99e56df8a1bc923b2dcaf4ce22",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "7df93929685b15edee2e",
+    "url": "/React_Portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "039a47dc00bd8cb38094",
+    "url": "/React_Portfolio/static/js/2.f9e28181.chunk.js"
+  },
+  {
+    "revision": "7df93929685b15edee2e",
+    "url": "/React_Portfolio/static/js/main.f955a435.chunk.js"
+  },
+  {
+    "revision": "5cfa4169311cdc2e60cf",
+    "url": "/React_Portfolio/static/js/runtime-main.b5409dca.js"
+  }
+]);
